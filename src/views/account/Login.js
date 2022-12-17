f4404d7c9b5bd8522e1c35a07d17a2ade4e838e3
Nolab <thibault.boutet@epita.fr>
2022-12-17 19:28:54 +0100

    link login and register form

--- src/views/account/Login.js
+++ src/views/account/Login.js
@@ -1,45 +1,22 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import LoginForm from 'components/Login/LoginForm';
+import { Link } from 'react-router-dom';
 
-export default function Login({ setToken }) {
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
+export default function Login({}) {
   const navigate = useNavigate();
 
   return (
     <div className='d-flex flex-column align-items-center justify-content-center h-100'>
       <h1>Connexion</h1>
-      <form>
-        <div className='d-flex flex-column align-items-center'>
-          <label>
-            <p>Email</p>
-            <input type='text' onChange={(e) => setUserName(e.target.value)} />
-          </label>
-          <label>
-            <p>Mot de passe</p>
-            <input
-              type='password'
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <div>
-            <button
-              type='button'
-              className='btn btn-info fixed-button'
-              onClick={() => {
-                setToken(true);
-                navigate('/dashboard');
-              }}>
-              Login
-            </button>
-          </div>
-        </div>
-      </form>
+      <LoginForm />
+      <div className='d-flex flex-row mt-4'>
+        <p className='mr-2'>Pas de compte ?</p>
+        <Link to='/register'>M'inscire</Link>
+      </div>
     </div>
   );
 }
 
-Login.propTypes = {
-  setToken: PropTypes.func.isRequired,
-};
+Login.propTypes = {};
