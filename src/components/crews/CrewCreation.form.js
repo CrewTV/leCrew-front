89e0effea385fd81b559832ab1613af156099c0e
Nolab <thibault.boutet@epita.fr>
2022-12-27 18:37:37 +0100

    add logic to add members

--- src/components/crews/CrewCreation.form.js
+++ src/components/crews/CrewCreation.form.js
@@ -1,67 +1,93 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FormGroup, Input, Label } from 'reactstrap';
-import { useFormik, Field, ErrorMessage, FieldArray } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
 import * as Yup from 'yup';
 
 export default function CrewCreationForm({}) {
-  const navigate = useNavigate();
-  const crewFormik = useFormik({
-    initialValues: {
-      name: '',
-      members: [
-        {
-          email: '',
-        },
-      ],
-    },
+  const initialValues = {
+    name: '',
+    members: [],
+  };
 
-    validationSchema: Yup.object({
-      name: Yup.string().required('Nom du Crew Requis'),
-      members: Yup.string(),
-    }),
-
-    onSubmit: async (values) => {
-      console.log(values);
-    },
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Nom du Crew Requis'),
+    members: Yup.array().of(
+      Yup.string('Adresse mail invalide').email('Adresse mail invalide')
+    ),
   });
 
-  console.log('Formik:', crewFormik);
+  const crewFormOnSubmit = (values) => {
+    console.log(values);
+  };
 
   return (
-    <form onSubmit={crewFormik.handleSubmit}>
-      <div className='d-flex flex-column align-items-center'>
-        <FormGroup
-          className={
-            crewFormik.touched.name && crewFormik.errors.name
-              ? 'has-error'
-              : null
-          }>
-          <Label
-            for={
-              crewFormik.touched.name && crewFormik.errors.name ? 'error' : null
-            }
-            className='control-label'>
-            Nom du Crew
-          </Label>
-          <Input
-            className='fixed_field'
-            type='text'
-            placeholder='Nom du Crew'
-            {...crewFormik.getFieldProps('name')}
-          />
-          {crewFormik.touched.name && crewFormik.errors.name ? (
-            <div className='mt-1 text-danger'>{crewFormik.errors.name}</div>
-          ) : null}
-        </FormGroup>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => crewFormOnSubmit(values)}>
+      {({ values, errors, setFieldValue }) => (
+        <Form>
+          <FormGroup className={errors.name ? 'has-error' : null}>
+            <Label
+              for={errors.email ? 'error' : null}
+              className='control-label'>
+              Nom du Crew
+            </Label>
+            <Input
+              className='fixed_field'
+              type='text'
+              placeholder='Nom du Crew'
+              onChange={(e) => setFieldValue('name', e.target.value)}
+            />
+            {errors.name ? (
+              <div className='mt-1 text-danger'>{errors.name}</div>
+            ) : null}
+          </FormGroup>
+          <FormGroup>
+            <Label className='control-label'>Membres</Label>
+            <FieldArray name='members'>
+              {({ insert, remove, push }) => (
+                <div>
+                  {values.members.length > 0 &&
+                    values.members.map((_, index) => (
+                      <div className='d-flex flex-row' key={index}>
+                        <Input
+                          className='fixed_field'
+                          placeholder='Email'
+                          type='email'
+                          onChange={(e) =>
+                            setFieldValue(`members.${index}`, e.target.value)
+                          }
+                        />
+                        <button
+                          type='button'
+                          className='btn btn-warning fixed-button'
+                          onClick={() => remove(index)}>
+                          X
+                        </button>
+                      </div>
+                    ))}
+                  {errors.members ? (
+                    <div className='mt-1 text-danger'>{errors.members}</div>
+                  ) : null}
+                  <button
+                    type='button'
+                    className='btn btn-success fixed-button'
+                    onClick={() => push('')}>
+                    Ajouter un membre
+                  </button>
+                </div>
+              )}
+            </FieldArray>
+          </FormGroup>
 
-        <div className='mt-50'>
           <button type='submit' className='btn btn-info fixed-button'>
             Création
           </button>
-        </div>
-      </div>
-    </form>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
