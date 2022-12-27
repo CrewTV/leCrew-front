0221ecc2d96c24d845820dcd15be209da7344d35
Nolab <thibault.boutet@epita.fr>
2022-12-27 18:48:13 +0100

    finish design of crew add form

--- src/components/crews/CrewCreation.form.js
+++ src/components/crews/CrewCreation.form.js
@@ -13,7 +13,9 @@ export default function CrewCreationForm({}) {
   const validationSchema = Yup.object({
     name: Yup.string().required('Nom du Crew Requis'),
     members: Yup.array().of(
-      Yup.string('Adresse mail invalide').email('Adresse mail invalide')
+      Yup.string()
+        .required('Adresse mail requise')
+        .email('Adresse mail invalide')
     ),
   });
 
@@ -22,72 +24,76 @@ export default function CrewCreationForm({}) {
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={(values) => crewFormOnSubmit(values)}>
-      {({ values, errors, setFieldValue }) => (
-        <Form>
-          <FormGroup className={errors.name ? 'has-error' : null}>
-            <Label
-              for={errors.email ? 'error' : null}
-              className='control-label'>
-              Nom du Crew
-            </Label>
-            <Input
-              className='fixed_field'
-              type='text'
-              placeholder='Nom du Crew'
-              onChange={(e) => setFieldValue('name', e.target.value)}
-            />
-            {errors.name ? (
-              <div className='mt-1 text-danger'>{errors.name}</div>
-            ) : null}
-          </FormGroup>
-          <FormGroup>
-            <Label className='control-label'>Membres</Label>
-            <FieldArray name='members'>
-              {({ insert, remove, push }) => (
-                <div>
-                  {values.members.length > 0 &&
-                    values.members.map((_, index) => (
-                      <div className='d-flex flex-row' key={index}>
-                        <Input
-                          className='fixed_field'
-                          placeholder='Email'
-                          type='email'
-                          onChange={(e) =>
-                            setFieldValue(`members.${index}`, e.target.value)
-                          }
-                        />
-                        <button
-                          type='button'
-                          className='btn btn-warning fixed-button'
-                          onClick={() => remove(index)}>
-                          X
-                        </button>
-                      </div>
-                    ))}
-                  {errors.members ? (
-                    <div className='mt-1 text-danger'>{errors.members}</div>
-                  ) : null}
-                  <button
-                    type='button'
-                    className='btn btn-success fixed-button'
-                    onClick={() => push('')}>
-                    Ajouter un membre
-                  </button>
-                </div>
+    <div className='d-flex flex-column align-items-center justify-content-center'>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values) => crewFormOnSubmit(values)}>
+        {({ values, errors, setFieldValue }) => (
+          <Form className='d-flex flex-column align-items-center justify-content-center'>
+            <FormGroup className={errors.name ? 'has-error' : null}>
+              <Label
+                for={errors.email ? 'error' : null}
+                className='control-label'>
+                Nom du Crew
+              </Label>
+              <Input
+                className='fixed_field'
+                type='text'
+                placeholder='Nom du Crew'
+                onChange={(e) => setFieldValue('name', e.target.value)}
+              />
+              {errors.name ? (
+                <div className='mt-1 text-danger'>{errors.name}</div>
+              ) : null}
+            </FormGroup>
+            <FormGroup>
+              {values.members.length > 0 && (
+                <Label className='control-label'>Membres</Label>
               )}
-            </FieldArray>
-          </FormGroup>
+              <FieldArray name='members'>
+                {({ remove, push }) => (
+                  <div className='d-flex flex-column justify-content-center'>
+                    {values.members.length > 0 &&
+                      values.members.map((_, index) => (
+                        <div className='d-flex flex-row mb-2' key={index}>
+                          <Input
+                            className='fixed_field'
+                            placeholder='Email'
+                            type='email'
+                            onChange={(e) =>
+                              setFieldValue(`members.${index}`, e.target.value)
+                            }
+                          />
+                          <button
+                            type='button'
+                            className='btn btn-warning btn-sm m-0 ml-1'
+                            onClick={() => remove(index)}>
+                            X
+                          </button>
+                        </div>
+                      ))}
+                    {errors.members ? (
+                      <div className='mt-1 text-danger'>{errors.members}</div>
+                    ) : null}
+                    <button
+                      type='button'
+                      className='btn btn-success fixed-button'
+                      onClick={() => push('')}>
+                      Ajouter un membre
+                    </button>
+                  </div>
+                )}
+              </FieldArray>
+            </FormGroup>
 
-          <button type='submit' className='btn btn-info fixed-button'>
-            Création
-          </button>
-        </Form>
-      )}
-    </Formik>
+            <button type='submit' className='btn btn-info fixed-button'>
+              Création
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
 
